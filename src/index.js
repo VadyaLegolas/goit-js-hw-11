6ecc7d1aef0888a8ea048b6baa789726f56b3a53
Vlad <vadya-85@mail.ru>
2023-09-25 00:39:21 +0200

    markup gallery

--- src/index.js
+++ src/index.js
@@ -12,15 +12,44 @@ import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import { Block } from 'notiflix/build/notiflix-block-aio';
 
 const refs = getRefs();
-const photosApiService = new PhotosApiService()
+const photosApiService = new PhotosApiService();
 
-refs.form.addEventListener('submit', onFormSubmit)
+refs.form.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(e){
-    e.preventDefault();
+function onFormSubmit(e) {
+  e.preventDefault();
 
-    photosApiService.query = e.currentTarget.elements.searchQuery.value;
-    photosApiService.fetchPhotos()
+  photosApiService.query = e.currentTarget.elements.searchQuery.value;
+  photosApiService.resetPage();
+  photosApiService.fetchPhotos().then(data => {
+    refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data))
+  }).catch(console.log);
+}
+
+function createMarkup(data){
+    return data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `<div class="photo-card">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views: ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments: ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads: ${downloads}</b>
+      </p>
+    </div>
+  </div>`).join('');
+
+}
+
+
+function clearMarkup(){
+    refs.gallery.innerHTML = ''
 }
 // var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
 
@@ -33,22 +62,4 @@ function onFormSubmit(e){
 //   behavior: "smooth",
 // });
 
-{
-  /* <div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div> */
-}
+
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -5,30 +5,44 @@ axios.defaults.headers.common['x-api-key'] =
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '39645635-7da43b24dbf787654135e35eb';
 
-
 export default class PhotosApiService {
   constructor() {
     this.searchQuery = '';
+    this.page = 1;
   }
 
-  get query(){
+  get query() {
     return this.searchQuery;
   }
 
-  set query(newQuery){
+  set query(newQuery) {
     this.searchQuery = newQuery;
   }
 
   fetchPhotos() {
     const params = new URLSearchParams({
-        key: API_KEY,
-        q: this.searchQuery,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      });
-      const url = `${BASE_URL}?${params}`;
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    });
+    const url = `${BASE_URL}?${params}`;
 
-      return fetch(url).then(res => res.json).then(console.log())
+    return fetch(url).then(res => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      this.incrementPage();
+      return res.json();
+    });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
   }
 }
