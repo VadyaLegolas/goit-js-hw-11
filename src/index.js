1ac3a64b68a5ad51d0c2a16242c7c33b527f9e44
Vlad <vadya-85@mail.ru>
2023-09-24 22:58:14 +0200

    add axios and notiflix

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_tUjJzgnldWMq1j0KXqo8xUPfkU0ZEQgWIOx6oI8Z4MApDV9Hp7RkOUWgC3xsOPtr';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
